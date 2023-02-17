21f4360f5e9d36c41ad10a1da3cd043c80f4eefc
Oubed <124842932+Oubed@users.noreply.github.com>
2023-02-17 21:03:30 +0000

    COMENTARIO

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
+//Agrega Libreria
 import { useState } from 'react';
-
+//primero se crea una función llamada, en este caso es 'square'
+//hace que esta función sea accesible fuera de este archivo
+//El boton significa que lo que viene despues se devuelve como un valor para la persona que llama de la funcion. 
 function Square({ value, onSquareClick }) {
   return (
     <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
+  //en este apartado podemos encontrar todas las funciones para poder crear las celdas
 }
 
-function Board({ xIsNext, squares, onPlay }) {
+function Board({ xIsNext, squares, onPlay }) { //puede llamar con la matriz de cuadros actualizada cada vez que un jugador hace un movimiento.
+  //funcion llena el cuadro con un valor declarado cuando detecte un 'click'
   function handleClick(i) {
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
+  //en este apartado podemos encontrar a los valores declarados que serán los mostrados en la pantalla
+  //cuando se detecte un 'click'
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = 'X';
@@ -21,7 +28,7 @@ function Board({ xIsNext, squares, onPlay }) {
     }
     onPlay(nextSquares);
   }
-
+//Sirve para indicar los estados del juego, si es el siguiente jugador o si se ha ganado la partida y quien ha ganadoo
   const winner = calculateWinner(squares);
   let status;
   if (winner) {
@@ -32,17 +39,21 @@ function Board({ xIsNext, squares, onPlay }) {
 
   return (
     <>
+     //Aqui encontramos a las celdas divididos por 3 filas y 3 columnas
       <div className="status">{status}</div>
+      //primera fila
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
         <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
+      //segunda fila
       <div className="board-row">
         <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
         <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
         <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
+      //tercera fila
       <div className="board-row">
         <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
@@ -51,10 +62,10 @@ function Board({ xIsNext, squares, onPlay }) {
     </>
   );
 }
-
+//tiene la función de mostrar el historial de los movimientos de los jugadores
 export default function Game() {
-  const [history, setHistory] = useState([Array(9).fill(null)]);
-  const [currentMove, setCurrentMove] = useState(0);
+  const [history, setHistory] = useState([Array(9).fill(null)]);// es una matriz con un solo elemento, que en sí mismo es una matriz de 9 nulls.
+  const [currentMove, setCurrentMove] = useState(0);//Es el que detecta el estado en el que se encuentra
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
@@ -68,6 +79,7 @@ export default function Game() {
     setCurrentMove(nextMove);
   }
 
+  //son las impresiones que mostrará el historial cuando detecte algún movimiento
   const moves = history.map((squares, move) => {
     let description;
     if (move > 0) {
@@ -76,6 +88,7 @@ export default function Game() {
       description = 'Go to game start';
     }
     return (
+      //boton deifinido, es donde aparecerán los mensajes de historial
       <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
@@ -83,6 +96,7 @@ export default function Game() {
   });
 
   return (
+    //muestra e imprime los movimientos pasados en un costado de las celdas
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
@@ -93,7 +107,7 @@ export default function Game() {
     </div>
   );
 }
-
+//son las formas en las que se pueden ganar o mejor dicho, cuales son las combinaciones ganadoras
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
@@ -105,6 +119,7 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6],
   ];
+  //ciclo para detectar los valores que se encuentren en las casillas y sí son iguales, mostrará el mensaje de ganador
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
